6e7674f048b9d06b2bf41b536a5f792d7bbeb274
pocandraspoc <pocandraspoc@gmail.com>
2017-10-30 12:09:33 +0100

    Update 00000.sh

--- 00000.sh
+++ 00000.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+sdate(){
+	SSTR="Start Time:"
+	SDATE=$(date)
+	echo $SSTR
+	echo $SDATE
+}
+
+edate(){
+	ESTR="End Time:"
+	EDATE=$(date)
+  echo $SSTR
+	echo $SDATE
+	echo $ESTR
+	echo $EDATE
+}
 
 ./00A00_A.sh
 ./00A00_tools.sh
 ./00A01_git.sh
 ./00A02_r.sh
 ./00A03_nvidia_pre.sh
+
+edate()
 
 reboot now
